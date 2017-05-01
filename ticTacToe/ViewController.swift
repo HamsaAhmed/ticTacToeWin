1de98868e74b41f3bee052350d5d715ccf709d69
user127697 <user127697@5-159-229-146.rdns.melbourne.co.uk>
2017-05-01 10:27:18 -0700

    Initial Commit

---
+++ ticTacToe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by user127697 on 5/1/17.
+//  Copyright © 2017 user127697. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
